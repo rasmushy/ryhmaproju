60869296ce9a5f9d48accec20085d04772d36f96
Rasmus Hyyppä <rasmus.hyyppa@metropolia.fi>
2021-12-02 11:59:55 +0200

    Putsasin koodia plus väsäsin .css leafletin popuppeihi

--- js.js
+++ js.js
@@ -1,10 +1,9 @@
 "use strict";
 const tanaan = new Date(); // aika jolla katotaan pizza paikat.
 const curAika = tanaan.getHours();
-let vkPaiva = tanaan.getDay() - 1 || 6;
-let arr = [];
+const vkPaiva = tanaan.getDay() - 1 || 6;
 const proxy = "https://cors-anywhere.herokuapp.com/";
-const map = L.map("map").setView([60.21, 24.95], 11.5);
+const map = L.map("map").setView([60.21, 24.95], 9);
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
@@ -12,6 +11,9 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 const maparticle = document.querySelector(".kartta");
 const nimi = document.querySelector("#nimi");
 /* const moreInfo = document.querySelector("#summary"); */
+// kustom markkerit
+const vIcon = L.divIcon({className: "vihrea-ikoni"});
+const pIcon = L.divIcon({className: "punainen-ikoni"});
 
 // Asetukset paikkatiedon hakua varten (valinnainen)
 const options = {
@@ -20,18 +22,53 @@ const options = {
   maximumAge: 0,
 };
 
-/* // kustom markkerit
-const vihreaikoni = L.divIcon({className: "vihrea-ikoni"});
-const punainenikoni = L.divIcon({className: "punainen-ikoni"}); */
+// funktio markerien tekoon
+function lisaaMarker(info) {
+  const summaryText = `
+  <h3 id="nimi"></h3>
+  <details>
+  <summary>Lisätiedot</summary>
+  <h4 id="osoite"></h4>
+  <p id="aukioloaika"></p>
+  <a id="urli" href="">Kotisivu</a>
+  </details>`;
+  if (curAika >= info.aukeeoloaika && curAika < info.aukioloaika) {
+    L.marker([info.Latitude, info.Longitude], {icon: pIcon})
+      .addTo(map)
+      .bindPopup(summaryText)
+      .on("click", () => {
+        document.querySelector("#nimi").innerHTML = info?.nimi || "";
+        document.querySelector("#osoite").innerHTML = info?.osoite || "";
+        document.querySelector("#aukioloaika").innerHTML = info?.aukeeoloaika || "";
+        document.querySelector("#aukioloaika").innerHTML += "-";
+        document.querySelector("#aukioloaika").innerHTML += info?.aukioloaika || "";
+        document.querySelector("#urli").href = info.urli || "javascript:void(0)";
+        document.querySelector("#lisatiedot").innerHTML = info?.lisatiedot || "";
+      });
+  } else {
+    L.marker([info.Latitude, info.Longitude], {icon: vIcon})
+      .addTo(map)
+      .bindPopup(summaryText)
+      .on("click", () => {
+        document.querySelector("#nimi").innerHTML = info?.nimi || "Damn son?";
+        document.querySelector("#osoite").innerHTML = info?.osoite || "";
+        document.querySelector("#aukioloaika").innerHTML = info?.aukeeoloaika || "";
+        document.querySelector("#aukioloaika").innerHTML += "-";
+        document.querySelector("#aukioloaika").innerHTML += info?.aukioloaika || "";
+        document.querySelector("#urli").href = info.urli || "javascript:void(0)";
+        document.querySelector("#lisatiedot").innerHTML = info?.lisatiedot || "";
+      });
+  }
+}
 
 function success(pos) {
   const crd = pos.coords;
+  map.setView([crd.latitude, crd.longitude], 15);
+  L.marker(crd.latitude, crd.longitude, "Olen tässä", {icon: pIcon}); // lisätään markkeri omaan lokaatioon
   getPizza().catch((e) => {
     console.log("Error");
     console.log(e);
   });
-  map.setView([crd.latitude, crd.longitude], 12);
-  L.marker(crd.latitude, crd.longitude, "Olen tässä"); // lisätään markkeri omaan lokaatioon
 }
 
 // Funktio, joka ajetaan, jos paikkatietojen hakemisessa tapahtuu virhe
@@ -46,29 +83,12 @@ async function getPizza() {
   const pizzaUrl = "http://open-api.myhelsinki.fi/v1/places/?tags_filter=Pizza";
   const response = await fetch(`${proxy}${pizzaUrl}`);
   const jsonData = await response.json();
-  console.log(jsonData.data);
-  hakuResults(jsonData.data);
-}
-
-/// for in loopilla monen objectin sisältä otetaan datat :3333
-
-function hakuResults(results) {
-  arr = results;
-  const summaryText = `
-  <h3 id="nimi"></h3>
-  <details>
-  <summary>Lisätiedot</summary>
-  <h4 id="osoite"></h4>
-  <p id="aukioloaika"></p>
-  <a id="urli" href="">Kotisivu</a>
-  </details>`;
-  /* maparticle.innerHTML = ""; // refreshaa sivun ku haetaan uudestaan. */ /* id="summary" */
-  maparticle.innerHTML += `
+  /*   maparticle.innerHTML += `
   <details>
   <summary>Lisätiedot</summary> 
   <p id="lisatiedot"></p>
-  </details>`;
-  arr.forEach(function (objData) {
+  </details>`; */
+  Array.from(jsonData.data).forEach(function (objData) {
     // jokasen objectin osalta seuraava forloop ->
     const info = {
       nimi: objData.name.fi,
@@ -80,17 +100,7 @@ function hakuResults(results) {
       lisatiedot: objData.description.body,
       urli: objData.info_url,
     };
-    L.marker([info.Latitude, info.Longitude])
-      .addTo(map)
-      .bindPopup(summaryText)
-      .on("click", () => {
-        document.querySelector("#nimi").innerHTML = info?.nimi || "Damn son?";
-        document.querySelector("#osoite").innerHTML = info?.osoite || "";
-        document.querySelector("#aukioloaika").innerHTML = info?.aukeeoloaika || "";
-        document.querySelector("#aukioloaika").innerHTML += "-";
-        document.querySelector("#aukioloaika").innerHTML += info?.aukioloaika || "";
-        document.querySelector("#urli").href = objData?.info_url || "javascript:void(0)";
-        document.querySelector("#lisatiedot").innerHTML = info?.lisatiedot || "";
-      });
+    lisaaMarker(info);
   });
+  console.log(jsonData.data);
 }

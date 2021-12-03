5199ca9a1b9dd9a47b8b11ddee4bf69051ba0337
Rasmus Hyyppä <rasmus.hyyppa@metropolia.fi>
2021-12-03 23:54:32 +0200

    .css searchbar added & tried to get it to function

--- js.js
+++ js.js
@@ -20,41 +20,44 @@ const options = {
   enableHighAccuracy: true,
   timeout: 5000,
   maximumAge: 0,
+  casesensitive: false,
 };
 
 // funktio markerien tekoon
 function lisaaMarker(open, closed, teksti, info) {
-  if (curAika >= open != false || (curAika < "23:00:00" && curAika < closed != true)) {
-    L.marker([info.Latitude, info.Longitude], {icon: pIcon})
+  if (curAika >= open != false && curAika > closed != false) {
+    L.marker([info.Latitude, info.Longitude], {icon: pIcon}, {title: info.nimi})
       .addTo(map)
       .bindPopup(teksti)
       .on("click", () => {
-        /*         document.querySelector("#map .hidden").style.visibility = "visible"; */
         document.querySelector("#Snimi").innerHTML = info?.nimi || "";
         document.querySelector("#Sosoite").innerHTML = info?.osoite || "";
         document.querySelector("#Surli").href = info?.urli || "javascript:void(0)";
       });
+    /*     pizzaData[i] = pizzaData[i].push(markerO);
+    console.log(pizzaData); */
   } else {
-    L.marker([info.Latitude, info.Longitude], {icon: vIcon}) /// tän hetkinen ongelma -> kun painaa blobia suoraan kun yksi avattu niin se ei lataa uudestaan tietoja.
+    L.marker([info.Latitude, info.Longitude], {icon: vIcon}, {title: info.nimi})
       .addTo(map)
       .bindPopup(teksti)
       .on("click", () => {
-        /*         document.querySelector("#map .hidden").style.visibility = "visible"; */
         document.querySelector("#Snimi").innerHTML = info?.nimi || "";
         document.querySelector("#Sosoite").innerHTML = info?.osoite || "";
         document.querySelector("#Surli").href = info?.urli || "javascript:void(0)";
       });
+    /*     pizzaData[i] = pizzaData[i].push(markerC);
+    console.log(pizzaData); */
   }
 }
 
 function success(pos) {
   const crd = pos.coords;
-  map.setView([crd.latitude, crd.longitude], 15);
-  L.marker(crd.latitude, crd.longitude, "Olen tässä", {icon: pIcon}); // lisätään markkeri omaan lokaatioon
   getPizza().catch((e) => {
     console.log("Error");
     console.log(e);
   });
+  map.setView([crd.latitude, crd.longitude], 15);
+  L.marker(crd.latitude, crd.longitude, "Olen tässä", {icon: pIcon}); // lisätään markkeri omaan lokaatioon
 }
 
 // Funktio, joka ajetaan, jos paikkatietojen hakemisessa tapahtuu virhe
@@ -69,39 +72,51 @@ async function getPizza() {
   const pizzaUrl = "http://open-api.myhelsinki.fi/v1/places/?tags_filter=Pizza";
   const response = await fetch(`${proxy}${pizzaUrl}`);
   const jsonData = await response.json();
+
+  /*   let i = 0; */
   Array.from(jsonData.data).forEach(function (objData) {
     // jokasen objectin osalta seuraava forloop ->
     const open = objData.opening_hours.hours[vkPaiva].opens; // tällä varmistetaan kumpi markkeri tulee mapille
     const closed = objData.opening_hours.hours[vkPaiva].closes; // verrattuna tämän hetkiseen aikaan
     const info = {
-      id: objData.id,
       nimi: objData.name.fi,
       osoite: objData.location.address.street_address,
       Latitude: objData.location.lat,
       Longitude: objData.location.lon,
       urli: objData.info_url,
-      /*       lisatiedot: objData.description.body, */
     };
+    /*     i += 1; */
     const teksti = `<h3 id="Snimi">${info.nimi}</h3>
+    <a id="Surli" href="${info.urli}">Kotisivu</a>
     <details>
     <summary>Lisätiedot</summary>
     <h4 id="Sosoite">${info.osoite}</h4>
     <p id="Saukioloaika">${open}-${closed}</p>
-    <a id="Surli" href="${info.urli}">Kotisivu</a>
     </details>`;
     lisaaMarker(open, closed, teksti, info);
   });
   console.log(jsonData.data);
 }
 
+function pizzaSearch(id) {
+  for (let j in pizzaData) {
+    const pizzaID = pizzaData[j].options.title;
+    if (pizzaID == id) {
+      pizzaData[j].openPopup();
+    }
+  }
+}
+
 // kun painat nappia -> tsegee onko inputboxissa mitään ja tästä syystä lähettää search toiminnon eteenpäin ja näyttää main contentin.
-let toiminto;
+let pId;
 hakuPainike.addEventListener("click", () => {
-  toiminto = hakuTxt.value;
-  if ((toiminto === "") != false) {
+  pId = hakuTxt.value;
+  if ((pId === "") != false) {
     return console.log("inputbox is empty");
   } else {
+    pizzaSearch(pId);
     console.log("searchaan");
+    console.log(pId);
   }
 });
 
@@ -110,11 +125,16 @@ hakuTxt.addEventListener("keyup", (e) => {
   if (e.keyCode === 13) {
     e.preventDefault();
     hakuPainike.click();
-    toiminto = hakuTxt.value;
-    if ((toiminto === "") != false) {
+    pId = hakuTxt.value;
+    if ((pId === "") != false) {
       return;
     } else {
+      pizzaSearch(pId);
       console.log("searchaan");
+      console.log(pId);
     }
   }
 });
+
+/* id: objData.id, */
+/* pizzaData[i] = info; */
